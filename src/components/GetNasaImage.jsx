5b605efa9ce424cc9b3dcdbca32fd0ae26192f18
n1999ck <millere9999@gmail.com>
2023-10-17 21:58:44 -0400

    Figuring out searching

---
+++ src/components/GetNasaImage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import axios from "axios";
+
+function GetNasaImage({nasaID}) {
+    
+
+    return(
+        <img src="../public/logo192.png" />
+    )
+}
+
+export default GetNasaImage
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,32 +1,62 @@
 import React, {useEffect, useState} from "react"
 import axios from "axios";
+import GetNasaImage from './GetNasaImage';
 
 //Gets user search query, uses it in NASA image search endpoint
 //https://images.nasa.gov/docs/images.nasa.gov_api_docs.pdf
 function Search() {
     const[searchInput, setSearchInput] = useState('');
+    const[results, setResults] = useState([]);
+    const[imageURL, setImageURL] = useState('');
 
     //URL with options - concatenate query
-    const urlBase = 'https://images-api.nasa.gov/search?media_type=image&page=1&year_end=1969&q='
+    const urlBase = 'https://images-api.nasa.gov/search?media_type=image&page=1&q='
 
     //https://react.dev/learn/state-a-components-memory
     function handleSearchInputChange(e){
-
-        console.log(e)
         setSearchInput(e.target.value);
     }
 
     function submitSearch(e) {
-        
         let apiURL = urlBase + searchInput;
-        window.open(apiURL)
-    }
+        console.log(apiURL)
+        axios.get(apiURL)
+            .then((response) => {
+                console.log("Got to response")
+                var index = 0
+                //console.log(response.data)
+                setResults(response.data.collection)
+                
+                if(results.metadata.total_hits > 100){
+                    index = (Math.floor(Math.random() * 100))
+                }else{
+                    index = (Math.floor(Math.random()* results.metadata.total_hits))
+                }
+                //console.log(index)
+                console.log(results.items[index])
+                //nasa id to get images from media asset manifest
+                console.log(results.items[index].data[0].nasa_id)
+                //console.log(results.items[index].data)
+                //console.log(results.items[index].links)
+            })
+            .catch(function(error){
+                console.log(error);
+            })
+        
+        }
+    
+    
+    useEffect(() => {
+        console.log("we're in the effect")
+    }, [results] )
 
     return(
         <React.Fragment>
-            <input type="search" name="imageSearch" id="searchBar" value={searchInput} onChange={handleSearchInputChange}/>
 
-            <button type="button" class="btn btn-primary" onClick={submitSearch}>Search</button>
+                <input type="search" name="imageSearch" id="searchBar" value={searchInput} onChange={handleSearchInputChange}/>
+                <button type="submit" class="btn btn-primary" onClick={submitSearch}>Search</button>
+                
+                
         </React.Fragment>
     )
 }
